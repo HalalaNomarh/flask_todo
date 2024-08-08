902e6e4281543e65f38dbc9dd3b9ceed69839dc8
HalalaNomarh <halalanomarh@gmail.com>
2024-08-08 14:53:21 +0200

    initial commit

---
+++ bubble sort.py
@@ -0,0 +1,16 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(n- i- 1):
+            if arr[j]>arr [j + 1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+list = [50,72, 1, 8, 13, 16, 20]
+bubble_sort(list)
+print(list)
+
+#insetion sort
+
+    
+    
+    
+    
